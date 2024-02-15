17f95f79b42b9fe45b786e58918129f9b1cb51ee
Colby Norwood <colbynorwood@colbys-mbp.main.local.una.edu>
2024-02-15 16:45:44 -0600

    server

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5500;
 const { MongoClient, ServerApiVersion } = require('mongodb');
+// const bodyParser = require('');
 
 // set the view engine to ejs
 let path = require('path');
@@ -12,8 +13,6 @@ app.set('views', path.join(__dirname, 'views'));
 // use res.render to load up an ejs view file
 
 let myTypeServer = "Type 6: Loyalist";
-
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(process.env.URI, {
@@ -29,21 +28,42 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
+    const result = await client.db("papa-database").collection("papa-collection").find().toArray();
+
+    console.log("papa-database result: ", result);
+
+    return result;
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
-run().catch(console.dir);
+//run().catch(console.dir);
+
+app.get('/read', async (req,res) => {
+
+  let dbResult = await run();
+  console.log("myResults:", dbResult[0].userName);
+
+
+
+
+  res.render('index', {
+    myTypeClient: myTypeServer,
+    myView: dbResult
+
+  });
+
+});
 
 
 
 app.get('/', function(req, res) {
 
   res.render('index', {
-   
     myTypeClient: myTypeServer 
 
   });
@@ -59,5 +79,5 @@ app.get('/send', function (req, res) {
 // app.listen(3000)
 
 app.listen(port, () => {
-  console.log(`nov app listening on port ${port}`)
+  console.log(`papa app listening on port ${port}`)
 })
