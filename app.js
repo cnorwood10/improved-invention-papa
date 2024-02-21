2ee6547665ce860d3478fcde8a17608b20299082
Colby Norwood <colbynorwood@Colbys-MacBook-Pro.local>
2024-02-20 23:04:50 -0600

    cleaned up code

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5500;
 const { MongoClient, ServerApiVersion } = require('mongodb');
-// const bodyParser = require('');
 
 // set the view engine to ejs
 let path = require('path');
@@ -12,7 +11,6 @@ app.set('views', path.join(__dirname, 'views'));
 
 // use res.render to load up an ejs view file
 
-let myTypeServer = "Type 6: Loyalist";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(process.env.URI, {
@@ -31,7 +29,7 @@ async function run() {
     // await client.db("admin").command({ ping: 1 });
     const result = await client.db("papa-database").collection("papa-collection").find().toArray();
 
-    console.log("papa-database result: ", result);
+    //console.log("papa-database result: ", result);
 
     return result;
 
@@ -41,15 +39,12 @@ async function run() {
     await client.close();
   }
 }
-//run().catch(console.dir);
 
-app.get('/read', async (req,res) => {
+app.get('/', async (req,res) => {
 
   let dbResult = await run();
-  console.log("myResults:", dbResult[0].userName);
 
-
-
+  console.log("myResults:", dbResult);
 
   res.render('index', {
     myTypeClient: myTypeServer,
@@ -58,25 +53,6 @@ app.get('/read', async (req,res) => {
   });
 
 });
-
-
-
-app.get('/', function(req, res) {
-
-  res.render('index', {
-    myTypeClient: myTypeServer 
-
-  });
-  
-});
-
-
-app.get('/send', function (req, res) {
-  
-    res.send('Hello World from Express <br><a href="/">home</a>')
-})
-
-// app.listen(3000)
 
 app.listen(port, () => {
   console.log(`papa app listening on port ${port}`)
